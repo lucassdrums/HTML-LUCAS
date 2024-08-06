c0ed98dd14611698f534526c8f23488059c35f60
Lucas <lucas.liverpool5@gmail.com>
2024-08-05 21:47:34 -0300

    Atualizacao de ex

---
+++ Python/controle_visitantes/usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+#class Usuario(AbstractBaseUser, PermissionsMixin);
+#email = models.EmailField(
+#verbose_name = "E-mail do usuario",
+#max_length = 100,
+#unique = True,
+#)
+#verbose_name = atributo que define um label ou uma descrição para o campo.
---
+++ Python/controle_visitantes/usuarios/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def usuarios(request):
+    return HttpResponse("Olá Mundo !!!")
+
+# Create your views here.
+
+
