0b604e908acb9c4fa327442506248a805a618d19
biblioteca.botafogo <biblioteca.botafogo@senacrjedu.local>
2024-08-28 17:57:11 -0300

    exercicio

---
+++ Python/controle/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\biblioteca.botafogo\HTML-LUCAS\python\controle\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- Python/controle_visitantes/controle_visitantes/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from usuarios.views import usuarios
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', usuarios, name='usuarios' )
-]
